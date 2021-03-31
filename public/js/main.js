6ed1fea3e20f826b4c0d669c356e105216fc277a
Zafar6699 <webzafar6699@gmail.com>
2021-03-31 18:30:19 +0500

    s

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,33 @@
 $(document).ready(function(){
 
-    $(".dropdown-li>a").click(function(){
-        
+    
+
+
+    $(".bt-dropdown").click(function(e){
+        let attr = $(this).attr("data-target");
+        $("#"+attr).slideToggle(0);
+        e.stopPropagation()
+    });
+
+    $("header div.profile-dropdown button").click(function(){
+        $(this).toggleClass("__active");
+    });
+
+    $(window).click(function(){
+        $(".my-dropdown").slideUp(0);
+        $("header div.profile-dropdown button").removeClass("__active");
+    })
+
+    
+    $(".bt-bars").click(function(){
+        $("nav.menu-nav").toggleClass("small-menu-nav");
+        $("nav.menu-nav").toggleClass("open-nav");
+        $("header.header").toggleClass("small-header");
+        $("div.view-content").toggleClass("small-view-content");
+    });
+
+
+    $("nav.open-nav .dropdown-li>a").click(function(){
         $(".dropdown-li>a").not(this).next().slideUp(200)
         $(".dropdown-li>a").not(this).removeClass("link__active");
         $(".dropdown-li>a").not(this).removeClass("a__open");
@@ -10,5 +36,5 @@ $(document).ready(function(){
         $(this).toggleClass("a__open");
         $(this).next().slideToggle(200)
     })
-
 });
+
